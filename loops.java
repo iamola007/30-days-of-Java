b2b8a113aa1127f3db529a0f0e7fff36b3fe16b8
Ola Oladapo <78447856+iamola007@users.noreply.github.com>
2024-11-02 12:52:39 +0100

    Create loops.java
    A program that uses for loop to create a multiplications tables.

---
+++ loops.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        
+        for(int x = 1; x <= 10; x++){
+        System.out.println(n + " x " + x + " = " + (n * x));
+    }
+    }
+}
